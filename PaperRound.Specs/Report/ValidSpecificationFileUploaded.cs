ec48088b1e66515d425a80e3fd7f75811bdf00f7
Jacob Polden <jacob.polden@gmail.com>
2017-02-04 09:57:15 +0000

    Added tests for happy path

---
+++ PaperRound.Specs/Report/ValidSpecificationFileUploaded.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace PaperRound.Specs.Report
+{
+    [Trait("A valid specification file is uploaded", "")]
+    public class ValidSpecificationFileUploaded
+    {
+        [Fact(DisplayName = "A message that the file is valid")]
+        void FileIsValid()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact(DisplayName = "Number of houses on a street")]
+        void NumberOfHouses()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact(DisplayName = "Number of houses on the left of the street")]
+        void NumberOfHousesOnTheLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact(DisplayName = "Number of houses on the right of the street")]
+        void NumberOfHousesOnTheRight()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
